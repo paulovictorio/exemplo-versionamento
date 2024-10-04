6d3ec40ec2a3c5c177ee5c831179c8d7994d1dfe
Paulo Victorio <paulocesarvictorio940@gmail.com>
2024-10-04 20:52:14 -0300

    index alt

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 
 app.get('/api', (req, res) => {
-  res.send('Hello world!');
+  res.send('Hello worlds!');
 });
 
 app.get('/api/test', (req, res) => {
